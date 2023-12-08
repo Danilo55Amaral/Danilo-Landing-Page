14b975e522d84747b1bff73a8d988679561ddbab
Danilo Amaral <danilo123amaral@gmail.com>
2023-12-08 15:15:11 -0300

    :wrench: chore: Ajustando Imagem / Criando responsividade seção de home

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,30 @@
+import Image from "next/image";
 import Banner from "./components/banner";
 
 export default function Home() {
   return (
     <>
-      <div className="flex">
-        <Banner src="/image-background.png" alt="Imagem de Fundo" />
-        <h1 className="text-white z-10 absolute justify-self-center">Ola mundo</h1>
+      <Banner src="/image-background.png" alt="Imagem de Fundo" />
+      <div className="flex items-center h-full justify-center">
+        <div className="flex flex-col-reverse lg:flex-row items-center justify-center w-full z-10 absolute">
+          <p className="text-white text-justify lg:w-1/3 w-full">
+            Seja bem-vindo ao meu portfólio, conheça alguns dos meus trabalhos
+            como freelancer e também alguns projetos que construir, meu objetivo
+            é transformar de forma positiva a vida das pessoas através da
+            tecnologia.
+          </p>
+          <Image
+            src="/image-astronauta.png"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto max-h-[40%] lg:max-h-[30%] w-auto max-w-[50%] lg:max-w-[20%]"
+            style={{
+              objectFit: "contain",
+            }}
+            alt="Astronauta"
+          />
+        </div>
       </div>
     </>
   );
