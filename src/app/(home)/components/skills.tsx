61338d081669d648a0923b4ea8000f5573deb7e0
Danilo Amaral <danilo123amaral@gmail.com>
2024-01-15 16:15:21 -0300

    :sparkles: feat: Componente Skills / Adicionando animação a home

--- src/app/(home)/components/skills.tsx
+++ src/app/(home)/components/skills.tsx
@@ -1,14 +1,142 @@
-
+import {
+  SiAngular,
+  SiBootstrap,
+  SiCss3,
+  SiDocker,
+  SiFigma,
+  SiFirebase,
+  SiGit,
+  SiGithub,
+  SiGnubash,
+  SiGoogleanalytics,
+  SiHtml5,
+  SiJavascript,
+  SiMongodb,
+  SiMysql,
+  SiNetlify,
+  SiNextdotjs,
+  SiNotion,
+  SiNpm,
+  SiPhp,
+  SiPython,
+  SiReact,
+  SiSlack,
+  SiStyledcomponents,
+  SiTailwindcss,
+  SiTrello,
+  SiTypescript,
+  SiVercel,
+} from "react-icons/si";
 
 export default function Skills() {
   return (
-    <div className="flex">
-      <h1 className="text-white">Minhas Habilidades</h1>
-      <div className="flex flex-col items-center gap-3">
-       
-        <span className="text-blue-900 font-semibold">
-          
-        </span>
+    <div className="flex flex-col gap-6 justify-around items-center">
+      <h1 className="text-white text-2xl font-bold">Minhas Habilidades</h1>
+      <div className="grid grid-cols-2 ml-7 font-semibold px-4 text-xl lg:grid-cols-5 lg:ml-0">
+        <ul className="inline-flex gap-1 my-1 lg:my-3 text-blue-700">
+          <SiReact className="mt-1 lg:text-3xl lg:mt-0 text-blue-700" />
+          React
+        </ul>
+        <ul className="inline-flex gap-1 my-1 lg:my-3 text-blue-700">
+          <SiNextdotjs className="mt-1 lg:text-3xl lg:mt-0 text-blue-700" />
+          Next
+        </ul>
+        <ul className="inline-flex gap-1 my-1 lg:my-3 text-blue-700">
+          <SiAngular className="mt-1 lg:text-3xl lg:mt-0 text-blue-700" />
+          Angular
+        </ul>
+        <ul className="inline-flex gap-1 my-1 lg:my-3 text-blue-700">
+          <SiHtml5 className="mt-1 lg:text-3xl lg:mt-0 text-blue-700" />
+          HTML5
+        </ul>
+        <ul className="inline-flex gap-1 my-1 lg:my-3 text-blue-700">
+          <SiCss3 className="mt-1 lg:text-3xl lg:mt-0 text-blue-700" />
+          CSS3
+        </ul>
+        <ul className="inline-flex gap-1 my-1 lg:my-3 text-blue-700">
+          <SiJavascript className="mt-1 lg:text-3xl lg:mt-0 text-blue-700" />
+          JavaScript
+        </ul>
+        <ul className="inline-flex gap-1 my-1 lg:my-3 text-blue-700">
+          <SiTypescript className="mt-1 lg:text-3xl lg:mt-0 text-blue-700" />
+          TypeScript
+        </ul>
+        <ul className="inline-flex gap-1 my-1 lg:my-3 text-blue-700">
+          <SiPython className="mt-1 lg:text-3xl lg:mt-0 text-blue-700" />
+          Python
+        </ul>
+        <ul className="inline-flex gap-1 my-1 lg:my-3 text-blue-700">
+          <SiPhp className="mt-1 lg:text-3xl lg:mt-0 text-blue-700" />
+          Php
+        </ul>
+        <ul className="inline-flex gap-1 my-1 lg:my-3 text-blue-700">
+          <SiTailwindcss className="mt-1 lg:text-3xl lg:mt-0 text-blue-700" />
+          Tailwind CSS
+        </ul>
+        <ul className="inline-flex gap-1 my-1 lg:my-3 text-blue-700">
+          <SiStyledcomponents className="mt-1 lg:text-3xl lg:mt-0 text-blue-700" />
+          Styled
+        </ul>
+        <ul className="inline-flex gap-1 my-1 lg:my-3 text-blue-700">
+          <SiBootstrap className="mt-1 lg:text-3xl lg:mt-0 text-blue-700" />
+          Bootstrap
+        </ul>
+        <ul className="inline-flex gap-1 my-1 lg:my-3 text-blue-700">
+          <SiGit className="mt-1 lg:text-3xl lg:mt-0 text-blue-700" />
+          Git
+        </ul>
+        <ul className="inline-flex gap-1 my-1 lg:my-3 text-blue-700">
+          <SiGithub className="mt-1 lg:text-3xl lg:mt-0 text-blue-700" />
+          Github
+        </ul>
+        <ul className="inline-flex gap-1 my-1 lg:my-3 text-blue-700">
+          <SiMysql className="mt-1 lg:text-3xl lg:mt-0 text-blue-700" />
+          MySQL
+        </ul>
+        <ul className="inline-flex gap-1 my-1 lg:my-3 text-blue-700">
+          <SiDocker className="mt-1 lg:text-3xl lg:mt-0 text-blue-700" />
+          Docker
+        </ul>
+        <ul className="inline-flex gap-1 my-1 lg:my-3 text-blue-700">
+          <SiFirebase className="mt-1 lg:text-3xl lg:mt-0 text-blue-700" />
+          Firebase
+        </ul>
+        <ul className="inline-flex gap-1 my-1 lg:my-3 text-blue-700">
+          <SiNpm className="mt-1 lg:text-3xl lg:mt-0 text-blue-700" />
+          npm
+        </ul>
+        <ul className="inline-flex gap-1 my-1 lg:my-3 text-blue-700">
+          <SiGnubash className="mt-1 lg:text-3xl lg:mt-0 text-blue-700" />
+          Bash
+        </ul>
+        <ul className="inline-flex gap-1 my-1 lg:my-3 text-blue-700">
+          <SiFigma className="mt-1 lg:text-3xl lg:mt-0 text-blue-700" />
+          Figma
+        </ul>
+        <ul className="inline-flex gap-1 my-1 lg:my-3 text-blue-700">
+          <SiVercel className="mt-1 lg:text-3xl lg:mt-0 text-blue-700" />
+          Vercel Cloud
+        </ul>
+        <ul className="inline-flex gap-1 my-1 lg:my-3 text-blue-700">
+          <SiNetlify className="mt-1 lg:text-3xl lg:mt-0 text-blue-700" />
+          Netlify Cloud
+        </ul>
+        <ul className="inline-flex gap-1 my-1 lg:my-3 text-blue-700">
+          <SiSlack className="mt-1 lg:text-3xl lg:mt-0 text-blue-700" />
+          Slack
+        </ul>
+        <ul className="inline-flex gap-1 my-1 lg:my-3 text-blue-700">
+          <SiTrello className="mt-1 lg:text-3xl lg:mt-0 text-blue-700" />
+          Trello
+        </ul>
+        <ul className="inline-flex gap-1 my-1 lg:my-3 text-blue-700">
+          <SiNotion className="mt-1 lg:text-3xl lg:mt-0 text-blue-700" />
+          Notion
+        </ul>
+        <ul className="inline-flex gap-1 my-1 lg:my-3 text-blue-700">
+          <SiGoogleanalytics className="mt-1 lg:text-3xl lg:mt-0 text-blue-700" />
+          Google Analytics
+        </ul>
       </div>
     </div>
   );
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,11 @@
+"use client";
+
 import Image from "next/image";
 import Banner from "./components/banner";
 import { Button } from "@/components/ui/button";
-import { Separator } from "@/components/ui/separator";
 import About from "./components/about";
 import Skills from "./components/skills";
+import { TypeAnimation } from "react-type-animation";
 
 export default function Home() {
   return (
@@ -12,6 +14,25 @@ export default function Home() {
       <section id="home" className="flex items-center h-full justify-center">
         <div className="flex flex-col-reverse lg:flex-row items-center justify-center w-full z-10 absolute">
           <div className="flex flex-col items-center gap-6 lg:w-1/3 w-full">
+            <h1 className="text-white font-bold text-center">
+              Prazer,{" "}
+              <span className="text-blue-700 text-2xl">Danilo Amaral 👨🏼‍💻</span>{" "}
+              <br />
+              <TypeAnimation
+                sequence={[
+                  "Desenvolvedor Web 🚀",
+                  1000,
+                  "Desenvolvedor React ⚛️",
+                  2000,
+                  "Desenvolvedor Front-end 👨🏼‍💻",
+                  3000,
+                ]}
+                wrapper="span"
+                cursor={true}
+                repeat={Infinity}
+                style={{ fontSize: "1em", display: "inline-block" }}
+              />
+            </h1>
             <p className="text-white font-semibold text-justify w-[90%]">
               Seja bem-vindo ao meu portfólio, conheça alguns dos meus trabalhos
               como freelancer e também alguns projetos que construir, minha
@@ -39,7 +60,7 @@ export default function Home() {
       <section id="about" className="mt-6">
         <About />
       </section>
-      <section id="skills" className="mt-6">
+      <section id="skills" className="mt-20">
         <Skills />
       </section>
     </>
