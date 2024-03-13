aa388980ccb5b80e92ceb189a8502d42a8410cd7
Danilo Amaral <danilo123amaral@gmail.com>
2024-03-13 15:34:37 -0300

    :sparkles: feat: Criando e implementando o componente CallCenterLink

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,13 @@
 import Link from "next/link";
 import { Separator } from "./ui/separator";
-import { GithubLogo, InstagramLogo, LinkedinLogo, WhatsappLogo } from "@phosphor-icons/react/dist/ssr";
+import {
+  GithubLogo,
+  InstagramLogo,
+  LinkedinLogo,
+  WhatsappLogo,
+} from "@phosphor-icons/react/dist/ssr";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { CallCenterLink } from "./call-center-link";
 
 export default function Footer() {
   return (
@@ -23,58 +29,73 @@ export default function Footer() {
           <Link href="#about" aria-label="Link para seção de Sobre do site">
             <span className="hover:text-blue-700">Sobre</span>
           </Link>
-          <Link href="#skills" aria-label="Link para seção de Habilidades do site">
+          <Link
+            href="#skills"
+            aria-label="Link para seção de Habilidades do site"
+          >
             <span className="hover:text-blue-700">Habilidades</span>
           </Link>
         </div>
-        <div className="flex flex-col gap-3 text-white font-semibold">
-          <Link href="#portfolio" aria-label="Link para seção de Portfolio do site">
+        <div className="flex flex-col gap-3 text-white font-semibold cursor-pointer">
+          <Link
+            href="#portfolio"
+            aria-label="Link para seção de Portfolio do site"
+          >
             <span className="hover:text-blue-700">Portfolio</span>
           </Link>
           <Link href="#" aria-label="Link para seção de Certificados do site">
             <span className="hover:text-blue-700">Certificados</span>
           </Link>
+          <div className="hover:text-blue-700">
+            <CallCenterLink />
+          </div>
         </div>
       </div>
 
       <Separator className="w-11/12" />
 
       <div className="flex gap-3 mt-14 text-white">
-        <Link href="https://www.instagram.com/danilo_kairois" aria-label="link para Instagram">
+        <Link
+          href="https://www.instagram.com/danilo_kairois"
+          aria-label="link para Instagram"
+        >
           <InstagramLogo
             size={26}
             weight="fill"
             className="hover:text-blue-700"
           />
         </Link>
-        <Link href="https://wa.me/message/ZAEPJ4ITGJ3WL1" aria-label="link para contato direto pelo WhatsApp">
+        <Link
+          href="https://wa.me/message/ZAEPJ4ITGJ3WL1"
+          aria-label="link para contato direto pelo WhatsApp"
+        >
           <WhatsappLogo
             size={26}
             weight="fill"
             className="hover:text-blue-700"
           />
         </Link>
-        <Link href="https://www.linkedin.com/in/danilo-gabriel-amaral-ferreira-305457195/" aria-label="link para o Linkedin">
+        <Link
+          href="https://www.linkedin.com/in/danilo-gabriel-amaral-ferreira-305457195/"
+          aria-label="link para o Linkedin"
+        >
           <LinkedinLogo
             size={26}
             weight="fill"
             className="hover:text-blue-700"
           />
         </Link>
-        <Link href="https://github.com/Danilo55Amaral" aria-label="link para o GitHub">
-          <GithubLogo
-            size={26}
-            weight="fill"
-            className="hover:text-blue-700"
-          />
+        <Link
+          href="https://github.com/Danilo55Amaral"
+          aria-label="link para o GitHub"
+        >
+          <GithubLogo size={26} weight="fill" className="hover:text-blue-700" />
         </Link>
       </div>
 
       <div className="text-white px-8 py-4 text-[0.625rem]">
         © 2024 Copyright{" "}
-        <span className="font-semibold text-blue-700 pl-1">
-          Danilo Amaral
-        </span>
+        <span className="font-semibold text-blue-700 pl-1">Danilo Amaral</span>
       </div>
     </div>
   );
